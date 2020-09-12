d021750702389aa8c57f0be108d362f6ea1b10da
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2020-09-12 12:19:20 -0500

    first commit lady swallowed

---
+++ README.md
@@ -0,0 +1,7 @@
+# TEALS-SelfChecks
+TEALS Programming Projects from 9/11/20
+
+Each Java Project is a different project number from the book, such as #1 Mississippi and #3 Lady who swallowed a fly
+
+
+
---
+++ TEALS-Chapter1-PP-3/src/com/hervey/app/LadySwallowedFly.java
@@ -0,0 +1,58 @@
+package com.hervey.app;
+
+public class LadySwallowedFly {
+	
+	static String absurdBird = "How absurd to swallow a bird!";
+	static String howCow = "I don't know how she swallowed a cow!";
+	//static String dondontFly + think = "I don't know why she swallowed a fly - I think she'll die!";
+	//static String dontFly + perhaps = "I don't know why she swallowed a fly - Perhaps she'll die!";
+	static String imagineCat = "Imagine that! She swallowed a cat!";
+	static String throatGoad = "She just opened her throat and swallowed a goat!";
+	static String birdSpider = "She swallowed the bird to catch the spider;";
+	static String catBird = "She swallowed the cat to catch the bird,";
+	static String cowGoat = "She swallowed the cow to catch the goat,";
+	static String dogCat = "She swallowed the dog to catch the cat,";
+	static String goatDog = "She swallowed the goat to catch the dog,";
+	static String spiderFly = "She swallowed the spider to catch the fly;";
+	static String wriggled = "That wriggled and jiggled and tickled inside her!";
+//	static String ladySwallowed + dog = "There was an old lady that swallowed a dog;";
+//	static String ladySwallowed + bird = "There was an old lady who swallowed a bird;";
+//	static String ladySwallowed + cat = "There was an old lady who swallowed a cat;";
+//	static String ladySwallowed + cow = "There was an old lady who swallowed a cow;";
+//	static String ladySwallowed + fly = "There was an old lady who swallowed a fly.";
+//	static String ladySwallowed + goat = "There was an old lady who swallowed a goat;";
+//	static String ladySwallowed + horse = "There was an old lady who swallowed a horse;";
+//	static String ladySwallowed + spider = "There was an old lady who swallowed a spider;";
+	static String hogDog = "What a hog, to swallow a dog!";
+	static String ladySwallowed = "There was an old lady who swallowed a";
+	static String dontFly = "I don't know why she swallowed a fly - ";
+	static String think = " I think she'll die!";
+	static String perhaps = " Perhaps she'll die!";
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(absurdBird);
+
+	}
+
+}
